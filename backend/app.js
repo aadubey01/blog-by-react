fc3791488938f76bc64ec74fe7c9a9eee48a6555
aadubey01 <91027094+aadubey01@users.noreply.github.com>
2023-12-03 16:29:00 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,13 @@ import cors from 'cors';
 
 
 const app = express();
-app.use(cors());
+app.use(cors(
+  {
+    origin:["https://deploy-mern-1whq.vercel.app"],
+    methods:["POST","GET"],
+    credentials:true
+  }
+));
 app.use(express.json());
 
 app.use("/api/user",router); 
